36dfb53242c40603e0d47598f8bd48ff0c0b9d5b
SurajSonawane2415 <surajsonawane0215@gmail.com>
2025-06-27 16:06:12 +0530

    Add devicetree bindings and updates for virtual DAI support

--- drivers/dai/virtual/virtual_dai.c
+++ drivers/dai/virtual/virtual_dai.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Intel Corporation.
+ * Copyright (c) 2025, Suraj Sonawane <surajsonawane0215@gmail.com@gmail.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -10,12 +10,11 @@
 #include <zephyr/pm/device_runtime.h>
 #include <string.h>
 
-#define DT_DRV_COMPAT sof_virtual_dai
+#define DT_DRV_COMPAT virtual_dai
 LOG_MODULE_REGISTER(virtual_dai, CONFIG_DAI_LOG_LEVEL);
 
 struct virtual_dai_data {
-	bool active;
-	uint8_t direction;
+	struct dai_config cfg;
 };
 
 static int virtual_dai_probe(const struct device *dev)
@@ -30,100 +29,92 @@ static int virtual_dai_remove(const struct device *dev)
 	return 0;
 }
 
-static int virtual_dai_config_set(const struct device *dev, const struct dai_config *cfg)
+static int virtual_dai_config_set(const struct device *dev,
+			  const struct dai_config *cfg,
+			  const void *bespoke_data)
 {
+	if (cfg->type != DAI_VIRTUAL) {
+		LOG_ERR("wrong DAI type: %d", cfg->type);
+		return -EINVAL;
+	}
+
+	LOG_INF("virtual_dai: config_set()");
+	return 0;
+}
+
+static int virtual_dai_config_get(const struct device *dev,
+			  struct dai_config *cfg,
+			  enum dai_dir dir)
+{
+	LOG_INF("virtual_dai: virtual_dai_config_get()");
 	struct virtual_dai_data *data = dev->data;
-	LOG_INF("virtual_dai: config_set() direction=%u", cfg->direction);
-	data->direction = cfg->direction;
+
+	/* dump content of the DAI configuration */
+	memcpy(cfg, &data->cfg, sizeof(*cfg));
+
 	return 0;
 }
 
-static int virtual_dai_config_get(const struct device *dev, struct dai_config *cfg)
-{
-	LOG_INF("virtual_dai: config_get()");
-	return 0;
-}
-
-static int virtual_dai_get_properties(const struct device *dev, struct dai_properties *prop)
+/* virtual_dai_get_properties It getting skipped because of dai_config_dma_ch skipped in dai_config
+will enable further, to add virtual dma related config.
+*/
+static const struct dai_properties
+	*virtual_dai_get_properties(const struct device *dev, enum dai_dir dir, int stream_id)
 {
 	LOG_INF("virtual_dai: get_properties()");
-	prop->fifo_address = 0;
-	prop->fifo_depth = 0;
-	prop->stream_capabilities = DAI_STREAM_PLAYBACK | DAI_STREAM_CAPTURE;
-	return 0;
+	return NULL;
 }
 
-static int virtual_dai_trigger(const struct device *dev, enum dai_dir dir, int cmd)
+static int virtual_dai_trigger(const struct device *dev,
+		       enum dai_dir dir,
+		       enum dai_trigger_cmd cmd)
 {
-	struct virtual_dai_data *data = dev->data;
-
 	switch (cmd) {
 	case DAI_TRIGGER_START:
-		data->active = true;
 		LOG_INF("virtual_dai: START (dir=%d)", dir);
-		break;
+		return 0;
 	case DAI_TRIGGER_STOP:
-		data->active = false;
 		LOG_INF("virtual_dai: STOP (dir=%d)", dir);
-		break;
+		return 0;
 	case DAI_TRIGGER_PAUSE:
 		LOG_INF("virtual_dai: PAUSE (dir=%d)", dir);
-		break;
-	case DAI_TRIGGER_RESUME:
-		LOG_INF("virtual_dai: RESUME (dir=%d)", dir);
-		break;
+		return 0;
 	case DAI_TRIGGER_PRE_START:
-	case DAI_TRIGGER_PRE_RELEASE:
-		LOG_INF("virtual_dai: PRE_START/RELEASE (dir=%d)", dir);
-		break;
+	case DAI_TRIGGER_COPY:
+		LOG_INF("virtual_dai: DAI_TRIGGER_COPY");
+		return 0;
 	default:
 		LOG_WRN("virtual_dai: Unknown trigger %d", cmd);
-		break;
+		return -EINVAL;
 	}
-	return 0;
+
+	CODE_UNREACHABLE;
 }
 
-static int virtual_dai_ts_config(const struct device *dev, struct dai_ts_cfg *cfg)
-{
-	LOG_INF("virtual_dai: ts_config()");
-	return 0;
-}
-
-static int virtual_dai_ts_start(const struct device *dev)
-{
-	LOG_INF("virtual_dai: ts_start()");
-	return 0;
-}
-
-static int virtual_dai_ts_stop(const struct device *dev)
-{
-	LOG_INF("virtual_dai: ts_stop()");
-	return 0;
-}
-
-static int virtual_dai_ts_get(const struct device *dev, struct dai_ts_data *tsd)
-{
-	ts_d->timestamp = k_uptime_get_32();
-	LOG_INF("virtual_dai: ts_get() = %u", tsd->timestamp);
-	return 0;
-}
-
-static const struct dai_driver_api virtual_dai_api = {
+static DEVICE_API(dai, virtual_dai_api) = {
 	.probe = virtual_dai_probe,
 	.remove = virtual_dai_remove,
 	.config_set = virtual_dai_config_set,
 	.config_get = virtual_dai_config_get,
 	.get_properties = virtual_dai_get_properties,
 	.trigger = virtual_dai_trigger,
-	.ts_config = virtual_dai_ts_config,
-	.ts_start = virtual_dai_ts_start,
-	.ts_stop = virtual_dai_ts_stop,
-	.ts_get = virtual_dai_ts_get,
 };
 
-static struct virtual_dai_data virtual_dai_data_inst;
+static int virtual_dai_init(const struct device *dev)
+{
+	return 0;
+}
 
-DEVICE_DT_INST_DEFINE(0, NULL, NULL,
-			&virtual_dai_data_inst, NULL,
-			POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
-			&virtual_dai_api);
+#define VIRTUAL_DAI_INIT(inst) \
+static struct virtual_dai_data_ virtual_dai_data_##inst = {					\
+	.cfg.type = DAI_VIRTUAL,						\
+	.cfg.dai_index = DT_INST_PROP_OR(inst, dai_index, 0),			\
+};										\
+\
+DEVICE_DT_INST_DEFINE(inst, \
+		      &virtual_dai_init, NULL, \
+		      &virtual_dai_data_##inst, NULL, \
+		      POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE, \
+		      &virtual_dai_api);
+
+DT_INST_FOREACH_STATUS_OKAY(VIRTUAL_DAI_INIT);
